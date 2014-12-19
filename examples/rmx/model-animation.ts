a93f3d3e089c1b25ccd783900dd58e7ea22d3eae
crobi <robin@dbservice.com>
2014-12-19 10:52:10 +0100

    PoseStack

--- examples/rmx/model-animation.ts
+++ examples/rmx/model-animation.ts
@@ -19,6 +19,40 @@ module rmx {
     }
 
     /**
+    * A stack of poses.
+    * Use if you need temporary poses, e.g., in a complex pose blending setup.
+    */
+    export class PoseStack {
+        private poses: Pose[];
+        private pointer: number;
+        private skeleton: Skeleton;
+
+        constructor(skeleton: Skeleton) {
+            this.poses = [];
+            this.skeleton = skeleton;
+            this.reset();
+        }
+
+        reset(): void {
+            this.pointer = 0;
+        }
+
+        push(): Pose {
+            while (this.poses.length <= this.pointer) {
+                this.poses.push(new Pose(this.skeleton));
+            }
+
+            var result = this.poses[this.pointer];
+            this.pointer++;
+            return result;
+        }
+
+        pop(): void {
+            this.pointer--;
+        }
+    }
+
+    /**
     * Stores the transformation of all skeleton bones (in composed matrix form)
     */
     export class SkeletonMatrices {
