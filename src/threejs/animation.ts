8244b99a8afbb546e7f5ee74692217c82605cf89
Robert Carnecky <robin@dbservice.com>
2015-03-18 19:24:25 +0100

    Correctly handle inverse bind matrices in three.js export

--- src/threejs/animation.ts
+++ src/threejs/animation.ts
@@ -30,18 +30,36 @@ module COLLADA.Threejs {
                 "length": animation.keyframes,
                 "fps": animation.fps,
                 "hierarchy": animation.tracks.map((track, index) => {
+
+                    // Get the actual default transformation of the current bone
+                    // This is not equal to the transformation stored in threejsBones[index]
+                    var localMatrix = bones[index].node.initialLocalMatrix;
+                    var default_pos = new Float32Array(3);
+                    var default_rot = new Float32Array(4);
+                    var default_scl = new Float32Array(3);
+                    COLLADA.MathUtils.decompose(localMatrix, default_pos, default_rot, default_scl);
+
+                    // Add all keyframes
                     var keys: any[] = [];
                     for (var k = 0; k < animation.keyframes; ++k) {
                         var key: any = { "time": roundTo(k * time_scale, 2) }
+
+                        // Keyframe data
                         if (track.pos) key.pos = vec3Key(track.pos, k, 4);
                         if (track.rot) key.rot = vec4Key(track.rot, k, 4);
                         if (track.scl) key.scl = vec3Key(track.scl, k, 3);
+
+                        // First and last keyframes must be complete
                         if (k == 0 || k == animation.keyframes - 1) {
-                            if (!track.pos) key.pos = threejsBones[index].pos;
-                            if (!track.rot) key.rot = threejsBones[index].rotq;
-                            if (!track.scl) key.scl = threejsBones[index].scl;
+                            if (!track.pos) key.pos = vec3Key(default_pos, 0, 4);
+                            if (!track.rot) key.rot = vec4Key(default_rot, 0, 4);
+                            if (!track.scl) key.scl = vec3Key(default_scl, 0, 3);
                         }
-                        keys.push(key);
+
+                        // Ignore empty keys
+                        if (key.pos || key.rot || key.scl) {
+                            keys.push(key);
+                        }
                     }
 
                     return {
